35b46ead6a7d477c4707d1a4d7307d2362e615fd
wanrabbae <wancode.msg@gmail.com>
2023-06-28 18:46:58 +0700

    Setup handle upload function to DO Space

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 require('dotenv').config()
 const express = require('express')
+const helmet = require('helmet')
 
 const app = express()
-const PORT = 5000 || process.env.PORT
+const PORT = process.env.PORT
 
 require('./config/db-conf')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static('public'))
+app.use(helmet())
 
 app.get('/', (req, res) => {
     res.send('Eggreat backend service started!!')
+})
+
+app.get('/test', (req, res) => {
+    res.json({
+        key: "value"
+    })
 })
 
 app.listen(PORT, () => {
---
+++ src/utils/handleUpload.js
@@ -0,0 +1,39 @@
+const multer = require('multer');
+const AWS = require('aws-sdk');
+const multerS3 = require('multer-s3');
+
+const spacesEndpoint = new AWS.Endpoint(process.env.DO_SPACE_ENDPOINT);
+const s3 = new AWS.S3({
+    endpoint: spacesEndpoint,
+    accessKeyId: process.env.DO_SPACE_KEY,
+    secretAccessKey: process.env.DO_SPACE_SECRET
+});
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.DO_SPACE_NAME,
+        acl: 'public-read',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: function (req, file, cb) {
+            cb(null, Date.now().toString() + '-' + file.originalname);
+        }
+    })
+});
+
+const uploadMultiple = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.DO_SPACE_NAME,
+        acl: 'public-read',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: function (req, file, cb) {
+            cb(null, Date.now().toString() + '-' + file.originalname);
+        }
+    })
+}).array('files', 7);
+
+module.exports = {
+    upload,
+    uploadMultiple
+}
