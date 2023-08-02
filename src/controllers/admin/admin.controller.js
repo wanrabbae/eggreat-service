733c571d4d963cda39a1e82f7ef303f553680793
wanrabbae <wancode.msg@gmail.com>
2023-08-02 13:50:26 +0700

    Feat update & delete admin list

--- src/controllers/admin/admin.controller.js
+++ src/controllers/admin/admin.controller.js
@@ -1,5 +1,5 @@
 import AdminService from '../../services/AdminService.js';
-import { handleUpload } from '../../utils/handleUpload.js';
+import { deleteFile, handleUpload } from '../../utils/handleUpload.js';
 import { hashPassword } from '../../utils/functions.js'
 
 const service = new AdminService();
@@ -42,6 +42,57 @@ export const createAdmin = async (req, res) => {
         await service.createAdmin({ foto, fullname, username, phone, nik, address, role, password: newPw })
 
         return res.jsonSuccessCreated("Data created")
+    } catch (error) {
+        return res.errorBadRequest(error.message)
+    }
+}
+
+export const updateAdmin = async (req, res) => {
+    const { fullname, username, phone, nik, address, role } = req.body
+    try {
+        let data = {
+            fullname,
+            username,
+            phone,
+            nik,
+            address,
+            role,
+        };
+
+        const findAdm = await service.getAdmin(req.params.id)
+
+        if (findAdm == null) return res.errorNotFound("Admin tidak ditemukan");
+
+        data["foto"] = findAdm.foto
+
+        if (req.file) {
+            const upload = await handleUpload(req.file)
+            data["foto"] = upload.Location
+            if (upload) await deleteFile(findAdm.foto)
+        }
+
+        if (req.body.password) data["password"] = await hashPassword(req.body.password)
+
+        await service.updateAdmin(data, req.params.id)
+
+        return res.jsonSuccess("Data updated")
+    } catch (error) {
+        return res.errorBadRequest(error.message)
+    }
+}
+
+export const deleteAdmin = async (req, res) => {
+    try {
+        const findAdm = await service.getAdmin(req.params.id)
+        if (findAdm == null) return res.errorNotFound("Admin tidak ditemukan");
+
+        if (findAdm.foto != null) {
+            await deleteFile(findAdm.foto)
+        }
+
+        await service.destroyAdmin(req.params.id)
+
+        return res.jsonSuccess("Data deleted")
     } catch (error) {
         return res.errorBadRequest(error.message)
     }
--- src/routes/admin/admin.routes.js
+++ src/routes/admin/admin.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { createAdmin, getAdminAll } from '../../controllers/admin/admin.controller.js'
+import { createAdmin, deleteAdmin, getAdminAll, updateAdmin } from '../../controllers/admin/admin.controller.js'
 import { verifyAdmin } from '../../middleware/verifyAdmin.js'
 import { upload } from '../../utils/handleUpload.js';
 
@@ -7,5 +7,7 @@ const router = Router();
 
 router.get("/admin/list", verifyAdmin, getAdminAll);
 router.post("/admin", verifyAdmin, upload.single('foto'), createAdmin);
+router.put("/admin/:id", verifyAdmin, upload.single('foto'), updateAdmin);
+router.delete("/admin/:id", verifyAdmin, deleteAdmin);
 
 export default router
